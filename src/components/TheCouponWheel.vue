a6898f8b618a747138340192e8fe69fad36d2831
huan5678 <huan5678@gmail.com>
2022-03-25 17:50:34 +0800

    [upload] couponWheel

---
+++ src/components/TheCouponWheel.vue
@@ -0,0 +1,33 @@
+<script>
+import { ref, onMounted } from 'vue';
+import useStore from '@/stores';
+
+export default {
+  setup(){
+    const wheel = ref(null);
+    const { adminDataStore } = useStore();
+    const { adminData, handleGetDataList } = adminDataStore;
+
+    onMounted(() => {
+      adminData.selectedTarget = 'coupon';
+      handleGetDataList();
+    })
+
+    const items = computed(()=>adminData.dataList);
+
+    function launchWheel(){
+      wheel.value.launchWheel();
+    }
+
+    return {
+      items,
+      wheel,
+      launchWheel,
+    };
+  },
+};
+</script>
+
+<template>
+  <Roulette ref="wheel" :items="items" @click="launchWheel" />
+</template>
