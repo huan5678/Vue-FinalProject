675b581d6a445c9683f7fcdc3d80654f98fb5d3a
huan5678 <huan678@gmail.com>
2022-03-29 02:48:36 +0800

    [upload] roulette icon

--- src/components/TheCouponWheel.vue
+++ src/components/TheCouponWheel.vue
@@ -53,7 +53,7 @@ export default {
   <div class="fixed right-0 bottom-0
   translate-x-1/4 -translate-y-1/4 tooltip" data-tip="驚喜大轉盤">
     <SvgLoader
-    name="wheel"
+    name="Roulette"
     class="scale-50 active:scale-75
     transition-all duration-500 ease-[cubic-bezier(0.18,0.89,0.32,1.28)]" />
   </div>
