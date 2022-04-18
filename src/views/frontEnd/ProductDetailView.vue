c19eb9098b4e4e26f9256feeeecb4c81969ad30e
huan5678 <huan5678@gmail.com>
2022-04-18 17:54:55 +0800

    [upload] product detail

--- src/views/frontEnd/ProductDetailView.vue
+++ src/views/frontEnd/ProductDetailView.vue
@@ -5,6 +5,7 @@ import {
   onMounted,
   reactive,
   onActivated,
+  onBeforeMount,
 } from 'vue';
 import { useRoute } from 'vue-router';
 
@@ -33,16 +34,31 @@ export default {
       }
     }
 
-    onMounted(() => {
+    onBeforeMount(() => {
       handleGetProductDetail(route.params.id);
       const products = computed(() => productList.productDetail);
+      console.log('before mount id=', route.params.id);
       handleImage(products);
+      console.log('before mount =', products);
+      console.log(productImage);
+    });
+
+    onMounted(() => {
+      // handleGetProductDetail(route.params.id);
+      const products = computed(() => productList.productDetail);
+      handleImage(products);
+      console.log('mounted = ', products);
+      console.log('mounted');
+      console.log(productImage);
     });
 
     onActivated(() => {
       handleGetProductDetail(route.params.id);
       const products = computed(() => productList.productDetail);
       handleImage(products);
+      console.log('activated id=', route.params.id);
+      console.log('activated');
+      console.log(productImage);
     });
 
     function handleUpdateCart(id, num) {
@@ -68,8 +84,9 @@ export default {
 
 <template>
   <section
-    class="flex overflow-hidden flex-col pb-4 space-y-4 bg-gray-50 rounded-md"
+    class="pb-4 bg-gray-50"
   >
+  <div class="container flex flex-col space-y-4">
     <AppLink to="/product" class="flex gap-3 items-center p-3 text-secondary-700">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
         <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147
@@ -78,13 +95,28 @@ export default {
       </svg>
     回上一頁
     </AppLink>
-    <div class="flex justify-between p-4">
-      <img
-        class="max-h-[300px]"
-        :src="products.imageUrl"
-        :alt="products.title"
-      />
-      <div class="flex-1 p-4 space-y-8">
+    <div class="flex gap-8 p-4">
+      <div class="flex flex-col space-y-2 md:w-1/2">
+        <div class="flex gap-4">
+          <img
+            class="object-scale-down w-3/4"
+            :src="productImage.mainImage ? productImage.mainImage : products.imageUrl"
+            :alt="products.title"
+          />
+          <div class="flex flex-col gap-2">
+            <img
+              v-for="img in products.imagesUrl"
+              :key="img"
+              class="object-cover flex-auto max-h-[25%] cursor-pointer"
+              @click="handleImageCtrl(img)"
+              @keypress="handleImageCtrl(img)"
+              :src="img"
+              :alt="products.title"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="space-y-8 flex-auto md:w-1/2">
         <div class="flex gap-4 items-center">
           <h2 class="mb-2 text-xl font-bold">
             {{ products.title }}
@@ -94,42 +126,6 @@ export default {
               {{ products.category }}
             </span>
           </h2>
-          <ul class="flex gap-1 items-center">
-            <li v-for="(star, idx) in 5" :key="star + new Date()">
-              <svg
-                v-show="products.rating > idx"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                class="w-6 h-6 text-primary-500"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173
-                  6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927
-                  0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83
-                  4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"
-                />
-              </svg>
-              <svg
-                v-show="products.rating <= idx"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                class="w-6 h-6 text-gray-400"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389
-                  2.256c.386.198.824-.149.746-.592l-.83-4.73
-                  3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513
-                  0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523
-                  3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0
-                  0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8
-                  2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565
-                  0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"
-                />
-              </svg>
-            </li>
-          </ul>
         </div>
 
         <ul class="space-y-4">
@@ -190,14 +186,6 @@ export default {
         </ul>
       </div>
     </div>
-    <div class="flex overflow-x-auto gap-4">
-      <img
-        v-for="img in products.imagesUrl"
-        :key="img"
-        class="object-cover flex-auto w-1/12 max-h-48"
-        :src="img"
-        :alt="products.title"
-      />
-    </div>
+  </div>
   </section>
 </template>
